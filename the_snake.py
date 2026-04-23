cc12d137ce0056738c5e5138340d3279bce006b4
Кабанов Михаил <mkabanov2009@gmail.com>
2026-04-23 22:30:50 +0300

    class Apple(GameObject):

--- the_snake.py
+++ the_snake.py
@@ -22,3 +22,27 @@ class GameObject:
         """
         pass
 
+ы
+    """
+    Класс Apple. Наследуется от GameObject.
+    Появляется в случайном месте поля.
+    """
+    apple_color = (255, 0, 0)
+
+    super().__init__(position=None, body_color=apple_color)
+
+    self.radomize_position()
+
+    def randomize_position(self):
+        """
+        Устанавливает случайные координаты для яблока.
+        """
+        max_x = 640 - 20
+        max_y = 480 - 20
+
+        x.random.randrage(0, max_x + 1, 20)
+        y.random.randrage(0, max_y + 1, 20)
+
+        self.position = (x, y)
+
+    def draw(self, surface):
